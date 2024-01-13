d9fd813858719610b15c4ad4b833c1c9eec0fb5d
Михаил Миноцкий <smoke-m@yandex.com>
2024-01-13 18:05:03 +0300

    add format to cosnstants

--- constants.py
+++ constants.py
@@ -1,6 +1,7 @@
 # Глобальные настройки логера
 BACKUP_COUNT = 5
 ENCODING = 'cp1251'
-LOG_FILE_FOLDER = 'logs/logfile.log'
+FORMAT = '%(asctime)s - %(levelname)s - %(message)s - %(name)s'
+LOGS_FOLDER = 'logs/logfile.log'
 LOGGER_NAME = 'vk-bot'
 MAX_BYTES = 50000000
--- logger_config.py
+++ logger_config.py
@@ -2,16 +2,16 @@ import logging
 import sys
 from logging.handlers import RotatingFileHandler
 
-from constants import BACKUP_COUNT, ENCODING, LOG_FILE_FOLDER, MAX_BYTES
+from constants import BACKUP_COUNT, ENCODING, FORMAT, LOGS_FOLDER, MAX_BYTES
 
 
 def init_globals_logging():
     """Инициализация глобальных натсроек логера."""
     logging.basicConfig(
-        format='%(asctime)s - %(levelname)s - %(message)s - %(name)s',
+        format=FORMAT,
         handlers=[
             logging.StreamHandler(sys.stdout),
-            RotatingFileHandler(LOG_FILE_FOLDER, encoding=ENCODING,
+            RotatingFileHandler(LOGS_FOLDER, encoding=ENCODING,
                                 maxBytes=MAX_BYTES, backupCount=BACKUP_COUNT)
         ]
     )
