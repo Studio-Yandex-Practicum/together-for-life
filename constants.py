19408d85c2dc867c0d280a924d9021f8dd3e59c8
Михаил Миноцкий <smoke-m@yandex.com>
2024-01-14 20:01:02 +0300

    add review edits

--- constants.py
+++ constants.py
@@ -1,7 +1,8 @@
 # Глобальные настройки логера
 BACKUP_COUNT = 5
-ENCODING = 'cp1251'
-FORMAT = '%(asctime)s - %(levelname)s - %(message)s - %(name)s'
+ENCODING = 'UTF-8'
+DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+FORMAT = '%(asctime)s - %(levelname)s - %(name)s - %(message)s'
 LOGS_FOLDER = 'logs/logfile.log'
 LOGGER_NAME = 'vk-bot'
 MAX_BYTES = 50000000
--- logger_config.py
+++ logger_config.py
@@ -2,13 +2,15 @@ import logging
 import sys
 from logging.handlers import RotatingFileHandler
 
-from constants import BACKUP_COUNT, ENCODING, FORMAT, LOGS_FOLDER, MAX_BYTES
+from constants import (BACKUP_COUNT, DATE_FORMAT, ENCODING, FORMAT,
+                       LOGS_FOLDER, MAX_BYTES)
 
 
 def init_globals_logging():
     """Инициализация глобальных натсроек логера."""
     logging.basicConfig(
         format=FORMAT,
+        datefmt=DATE_FORMAT,
         handlers=[
             logging.StreamHandler(sys.stdout),
             RotatingFileHandler(LOGS_FOLDER, encoding=ENCODING,
