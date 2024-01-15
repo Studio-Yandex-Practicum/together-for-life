e37d7f26c916b30e0d3bb793939cbf56230f3098
Михаил Миноцкий <smoke-m@yandex.com>
2024-01-15 12:25:13 +0300

    uninstall requirements

--- constants.py
+++ constants.py
@@ -1,8 +1,8 @@
 # Глобальные настройки логера
 BACKUP_COUNT = 5
-ENCODING = 'UTF-8'
-DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
-FORMAT = '%(asctime)s - %(levelname)s - %(name)s - %(message)s'
-LOGS_FOLDER = 'logs/logfile.log'
-LOGGER_NAME = 'vk-bot'
+ENCODING = "UTF-8"
+DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+FORMAT = "%(asctime)s - %(levelname)s - %(name)s - %(message)s"
+LOGS_FOLDER = "logs/logfile.log"
+LOGGER_NAME = "vk-bot"
 MAX_BYTES = 50000000
--- logger_config.py
+++ logger_config.py
@@ -2,8 +2,14 @@ import logging
 import sys
 from logging.handlers import RotatingFileHandler
 
-from constants import (BACKUP_COUNT, DATE_FORMAT, ENCODING, FORMAT,
-                       LOGS_FOLDER, MAX_BYTES)
+from constants import (
+    BACKUP_COUNT,
+    DATE_FORMAT,
+    ENCODING,
+    FORMAT,
+    LOGS_FOLDER,
+    MAX_BYTES,
+)
 
 
 def init_globals_logging():
@@ -13,7 +19,11 @@ def init_globals_logging():
         datefmt=DATE_FORMAT,
         handlers=[
             logging.StreamHandler(sys.stdout),
-            RotatingFileHandler(LOGS_FOLDER, encoding=ENCODING,
-                                maxBytes=MAX_BYTES, backupCount=BACKUP_COUNT)
-        ]
+            RotatingFileHandler(
+                LOGS_FOLDER,
+                encoding=ENCODING,
+                maxBytes=MAX_BYTES,
+                backupCount=BACKUP_COUNT,
+            ),
+        ],
     )
