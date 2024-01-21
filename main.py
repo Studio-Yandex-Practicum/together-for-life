e3c64d1d85a44ea0aa390398e186eea64c61553a
Михаил Миноцкий <smoke-m@yandex.com>
2024-01-21 17:50:33 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+import logging
+
+from dotenv import load_dotenv
+
+from constants import LOGGER_NAME
+from logger_config import init_globals_logging
+from vkbot import VKBot
+
+load_dotenv()
+
+logger = logging.getLogger(LOGGER_NAME)
+logger.setLevel(logging.DEBUG)
+
+if __name__ == '__main__':
+    init_globals_logging()
+    try:
+        logger.debug(f'Запуск Бота - {LOGGER_NAME}')
+        bot_vk_chat = VKBot(os.getenv('TOKEN'))
+        bot_vk_chat.vkbot_up()
+    except Exception as error:
+        logger.error(f'Ошибка бота {LOGGER_NAME}: {error}')
