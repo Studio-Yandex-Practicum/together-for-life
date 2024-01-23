c6271365aae3f7ee0330e17360f65c5a4ddd37b6
Ermachkova Julia <miscanth@gmail.com>
2024-01-23 12:59:15 +0300

    Добавила csv-файл, utils.py - описан режим чтения

---
+++ utils.py
@@ -0,0 +1,26 @@
+import csv
+
+
+PATH_TO_DATA= 'data/menu.csv'
+
+
+def get_menu_dict():
+    with open((PATH_TO_DATA), mode='r') as file:
+        reader = csv.reader(file)
+        header = next(reader)
+        menu_dict = {}
+        for row in reader:
+            menu_dict[row[0]] = row[1]
+        return menu_dict
+
+
+def get_menu_labels():
+    return list(get_menu_dict())
+
+
+def get_message(button_name: str):
+    message = ""
+    for name in get_menu_dict():
+        if button_name == name:
+            message = get_menu_dict()[name]
+            return message
