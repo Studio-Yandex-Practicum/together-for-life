a608518d20989e1f435b90dc0d544964821ffe25
Ermachkova Julia <miscanth@gmail.com>
2024-01-25 00:12:58 +0300

    Сделала метод write_file приватным

--- utils.py
+++ utils.py
@@ -19,42 +19,42 @@ class DataManager:
             reader = csv.DictReader(file)
             for row in reader:
                 data.append(row)
-        self._menu = data
+        self.__menu = data
 
     def get_menu_labels(self) -> list[str]:
         """Возвращает список лейблов для всех кнопок"""
         labels_list = []
-        for row in self._menu:
+        for row in self.__menu:
             labels_list.append(row["Заголовок(меню)"])
         return labels_list
 
     def get_message(self, label: str):
         """Возвращает сообщение для заданной кнопки по лейблу"""
-        for row in self._menu:
+        for row in self.__menu:
             if row["Заголовок(меню)"] == label:
                 return row["Информация"]
 
-    def write_file(self):
+    def __write_file(self):
         """Метод перезаписи csv-файла"""
         with open(filename, "w", newline="", encoding="utf-8") as csvfile:
             fieldnames = ["Заголовок(меню)", "Информация"]
             writer = csv.writer(csvfile)
             writer.writerow(fieldnames)
             new_menu_list = []
-            for row in self._menu:
+            for row in self.__menu:
                 new_menu_list.append(row.values())
             writer.writerows(new_menu_list)
 
     def edit_message(self, label: str, new_message: str):
         """Изменение сообщения/информации для выбранной кнопки"""
-        for row in self._menu:
+        for row in self.__menu:
             if row["Заголовок(меню)"] == label:
                 row["Информация"] = new_message
-        self.write_file()
+        self.__write_file()
 
     def edit_label(self, label: str, new_label: str):
         """Изменение названия/лейбла для выбранной кнопки"""
-        for row in self._menu:
+        for row in self.__menu:
             if row["Заголовок(меню)"] == label:
                 row["Заголовок(меню)"] = new_label
-        self.write_file()
+        self.__write_file()
