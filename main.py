1ae43307422d7e68fd3579321a7c85d194275325
Михаил Миноцкий <smoke-m@yandex.com>
2024-01-25 12:57:43 +0300

    edits rewu v10

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 import time
 
 from dotenv import load_dotenv
+from vk_api.exceptions import ApiError
 
 from constants import RELOAD_TIME
 from logger_config import init_globals_logging
@@ -17,11 +18,14 @@ logger = logging.getLogger(__name__)
 
 def main():
     """Функция работы бота."""
-    bot_vk_chat = VKBot(os.getenv("VK_TOKEN"))
     while True:
         logger.debug("Запуск Бота.")
         try:
+            bot_vk_chat = VKBot(os.getenv("VK_TOKEN"))
             bot_vk_chat.vkbot_up()
+        except ApiError as error:
+            logger.error(f"Ошибка токена бота: {error}.")
+            break
         except Exception as error:
             logger.error(f"Ошибка бота: {error}.")
             time.sleep(RELOAD_TIME)
