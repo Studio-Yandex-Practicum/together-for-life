b8db65f779812ca8627ec2e1da9fdf4662297f76
Ermachkova Julia <miscanth@gmail.com>
2024-01-26 15:33:16 +0300

    Добавила логгирование в блок try..except при чтении и записи файла

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 """Модуль класса реализации чтения и записи файла csv."""
 
 import csv
+import sys
+import logging
 from pathlib import Path
 
 from constants import ENCODING, MENU_FILE_NAME, PATH_TO_DATA
@@ -9,6 +11,8 @@ from constants import ENCODING, MENU_FILE_NAME, PATH_TO_DATA
 BASE_DIR = Path(__file__).resolve().parent
 filename = BASE_DIR / PATH_TO_DATA / MENU_FILE_NAME
 
+logger = logging.getLogger(__name__)
+
 
 class MenuManager:
     """Класс формирования меню кнопок и их сообщений"""
@@ -16,13 +20,17 @@ class MenuManager:
     def __init__(self):
         """Чтение данных из csv-файла в список словарей"""
         data = []
-        with open(filename, "r", newline="", encoding=ENCODING) as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                data.append(row)
-        self.__key_label = reader.fieldnames[0]
-        self.__key_message = reader.fieldnames[1]
-        self.__menu = data
+        try:
+            with open(filename, "r", newline="", encoding=ENCODING) as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    data.append(row)
+            self.__key_label = reader.fieldnames[0]
+            self.__key_message = reader.fieldnames[1]
+            self.__menu = data
+        except IOError as error:
+            logger.error(f"Ошибка чтения файла: {error}.")
+            sys.exit()
 
     def get_menu_labels(self) -> list[str]:
         """Возвращает список лейблов для всех кнопок"""
@@ -43,14 +51,17 @@ class MenuManager:
 
     def __write_file(self):
         """Метод перезаписи csv-файла"""
-        with open(filename, "w", newline="", encoding=ENCODING) as csvfile:
-            fieldnames = [self.__key_label, self.__key_message]
-            writer = csv.writer(csvfile)
-            writer.writerow(fieldnames)
-            new_menu_list = []
-            for row in self.__menu:
-                new_menu_list.append(row.values())
-            writer.writerows(new_menu_list)
+        try:
+            with open(filename, "w", newline="", encoding=ENCODING) as csvfile:
+                fieldnames = [self.__key_label, self.__key_message]
+                writer = csv.writer(csvfile)
+                writer.writerow(fieldnames)
+                new_menu_list = []
+                for row in self.__menu:
+                    new_menu_list.append(row.values())
+                writer.writerows(new_menu_list)
+        except IOError as error:
+            logger.error(f"Ошибка чтения файла для записи: {error}.")
 
     def edit_message(self, label: str, new_message: str):
         """Изменение сообщения/информации для выбранной кнопки"""
